029188f9029560ce6289bce0c8b0231f3141d2f7
Roberto Lara <rlara@MacBook-Pro-de-Roberto.local>
2017-07-17 13:01:34 -0500

    Add Body Parser

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.get('/_health', function(req, res) {
     res.status(200);
-    res.send('Hello!!');
+    res.send('Health!');
 });
+
+app.use(bodyParser.json({ type : 'application/json'}));
 
 
 app.listen(3000);
