42a42ab80afd743b929115ec86700352d4cccfb3
Roberto Lara <rlara@MacBook-Pro-de-Roberto.local>
2017-07-17 20:11:04 -0500

    Add ESLint - airbnb

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 
-var urlmethods = require('./urlmethods.js');
+const app = express();
 
-app.get('/_health', function(req, res) {
-    res.status(200);
-    res.send('Health!');
+const urlmethods = require('./urlmethods.js');
+
+app.get('/_health', (req, res) => {
+  res.status(200);
+  res.send('Health!');
 });
 
-app.use(bodyParser.json({ type : 'application/json'}));
+app.use(bodyParser.json({ type: 'application/json' }));
 
 
 app.post('/urls', urlmethods.addNewUrl);
--- urlmethods.js
+++ urlmethods.js
@@ -1,36 +1,29 @@
+const URL_DB = [];
 
-var URL_DB = []
 
-
-addNewUrl = (req, res) => {
-    var index = URL_DB.findIndex( function(current){
-        return current.longUrl == req.body.url;
+const addNewUrl = (req, res) => {
+  const index = URL_DB.findIndex(current => (current.longUrl === req.body.url));
+  if (index < 0) {
+    const short = Math.random().toString(36).substring(2, 7);
+    URL_DB.push({
+      longUrl: req.body.url,
+      shortUrl: short,
     });
-    if( index<0 ){
-        var short = Math.random().toString(36).substring(2,7);
-        URL_DB.push({
-            longUrl : req.body.url,
-            shortUrl : short
-        });
-        res.status(201);
-        console.log('Url added to DB');
-        res.send({
-            "longUrl" : req.body.url,
-            "shortUrl" : short
-        });
-    }
-    else{
-        res.status(304);
-        console.log('Url founded in DB');
-        res.send(URL_DB[index]);
-    }
+    res.status(201);
+    res.send({
+      longUrl: req.body.url,
+      shortUrl: short,
+    });
+  } else {
+    res.status(304);
+    res.send(URL_DB[index]);
+  }
 };
 
-getUrls = (req, res) => {
-    res.status(200);
-    res.send(URL_DB);
+const getUrls = (req, res) => {
+  res.status(200);
+  res.send(URL_DB);
 };
-
 
 
 module.exports.addNewUrl = addNewUrl;
